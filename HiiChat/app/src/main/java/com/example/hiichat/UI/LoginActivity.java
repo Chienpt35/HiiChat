3454444597dbb286b91395baa38b6ba117440221
Chienpt35 <57400423+Chienpt35@users.noreply.github.com>
2020-04-19 19:36:44 +0700

    123

---
+++ HiiChat/app/src/main/java/com/example/hiichat/UI/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.hiichat.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.hiichat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
