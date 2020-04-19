737fc307291435745c1c69dcedd8310fc8065374
Chienpt35 <57400423+Chienpt35@users.noreply.github.com>
2020-04-19 22:51:17 +0700

    123

--- HiiChat/app/src/main/java/com/example/hiichat/UI/LoginActivity.java
+++ HiiChat/app/src/main/java/com/example/hiichat/UI/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.hiichat.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.hiichat.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickLogin(View view) {
+    }
+
+    public void clickResetPassword(View view) {
+    }
+
+    public void clickRegisterLayout(View view) {
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
     }
 }
---
+++ HiiChat/app/src/main/java/com/example/hiichat/UI/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.hiichat.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.hiichat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void clickRegister(View view) {
+    }
+
+    public void clickBackLogin(View view) {
+    }
+}
